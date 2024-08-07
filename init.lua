8e95fd0c68c0fb201f143a40727ca7dd6f9a291d
Ricardo de Arruda <settiricardo@gmail.com>
2024-08-07 19:02:16 +0100

    revert fzflua

--- init.lua
+++ init.lua
@@ -356,46 +356,21 @@ require('lazy').setup {
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-T>.
           -- map('gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
 
-          -- Find references for the word under your cursor.
           -- map('gr', require('telescope.builtin').lsp_references, 'Goto References')
 
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
           -- map('gI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
 
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
           -- map('gt', require('telescope.builtin').lsp_type_definitions, 'Goto Type definition')
 
-          -- Opens a popup that displays documentation about the word under your cursor
-          --  See `:help K` for why this keymap
-          -- map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
           -- map('<leader>fs', require('telescope.builtin').lsp_document_symbols, 'Lists LSP document symbols in the current buffer')
-
-          -- Fuzzy find all the symbols in your current workspace
-          --  Similar to document symbols, except searches over your whole project.
-          -- TODO: Move
           -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-          -- Rename the variable under your cursor
-          --  Most Language Servers support renaming across files, etc.
           map('<leader>lr', vim.lsp.buf.rename, 'LSP Rename')
 
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
           map('<leader>la', vim.lsp.buf.code_action, 'LSP Action')
 
           -- Git
@@ -1199,7 +1174,7 @@ require('lazy').setup {
     },
     keys = {
       -- stylua: ignore start
-      -- { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({}) end, desc = "Open Yank History" },
+      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({}) end, desc = "Open Yank History" },
       { 'y',         '<Plug>(YankyYank)',                                                          mode = { 'n', 'x' },                                  desc = 'Yank text' },
       { 'p',         '<Plug>(YankyPutAfter)',                                                      mode = { 'n', 'x' },                                  desc = 'Put yanked text after cursor' },
       { 'P',         '<Plug>(YankyPutBefore)',                                                     mode = { 'n', 'x' },                                  desc = 'Put yanked text before cursor' },
@@ -1273,8 +1248,8 @@ require('lazy').setup {
           center = {
             -- { action = Util.telescope("files"),                                    desc = " Find file",       icon = " ", key = "f" },
             { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
-            { action = [[require('fzf-lua').oldfiles()]], desc = " Recent files", icon = " ", key = "r" },
-            { action = "[[require('fzf-lua').live_grep()]]", desc = " Find text", icon = " ", key = "g" },
+            -- { action = [[require('fzf-lua').oldfiles()]], desc = " Recent files", icon = " ", key = "r" },
+            -- { action = "[[require('fzf-lua').live_grep()]]", desc = " Find text", icon = " ", key = "g" },
             { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
             -- { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
             { action = "e ~/.config/nvim/init.lua", desc = " Open Neovim config", icon = " ", key = "c" },
